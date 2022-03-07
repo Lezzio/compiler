1b60e0d4be593d430a0043f9930214eb8b8d7f54
Maxime-Boyer <maxime.boyer@insa-lyon.fr>
2022-03-07 11:52:26 +0100

    TS work in progress

---
+++ compiler/symbol-table/Symbol.cpp
@@ -0,0 +1,50 @@
+#include "Symbol.h"
+
+Symbol::Symbol(string name, string type, int address, int additionnal){
+    this.name = name;
+    this.type = type;
+    this.address = address;
+    this.additionnal = additionnal;
+}
+
+
+void Symbol::setName(string name){
+
+}
+
+void Symbol::setScope(string scope){
+
+}
+
+void Symbol::setType(int type){
+
+}
+
+void Symbol::setAddress(int address){
+
+}
+
+void Symbol::setAdditional(int additional){
+
+}
+
+string Symbol::getName(){
+
+}
+
+string Symbol::getScope(){
+
+}
+
+string Symbol::getType(){
+
+}
+
+int Symbol::getAddress(){
+
+}
+
+int Symbol::getAdditional(){
+
+}
+
---
+++ compiler/symbol-table/testSTtemporel.cpp
@@ -0,0 +1,12 @@
+// compilation: g++ testSTtemporel.cpp SymbolTable.cpp -o test
+
+#include "SymbolTable.h"
+
+int main(int argn, const char **argv){
+    SymbolTable ST;
+    // ST->addSymbol("a", -1, 0, 0, -1);
+    // ST->addSymbol("b", -1, 4, 4, 3);
+    // ST->addSymbol("a", 1, 0, 0, -1);
+    // ST->print_dictionary();
+}
+
