1f9ac6021bf5f0104f50f4e18dcd7b2ae2814d7f
bdeleglise <benoit.deleglise@outlook.fr>
2022-03-21 11:46:48 +0100

    ty vscode ...

--- compiler/test.c
+++ compiler/test.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <inttypes.h>
 int main() {
 int b =2;
@@ -13,19 +12,4 @@ j = 9;
 int z,y,w;
 int r = 2+3;
 return r;
-=======
-#include <inttypes.h>
-int main() {
-int b =2;
-int a = b;
-int c = 3;
-c = b;
-int d;
-int e;
-e = b;
-int j;
-j = 9;
-int z,y,w;
-return j;
->>>>>>> a316bc88c06c1b9ce4999385d801760acde592a5
 }
