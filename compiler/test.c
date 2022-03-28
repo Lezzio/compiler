1658284630031c6797b123a16c5d19234f1d0fb1
Allan Guigal <allan.guigal0@gmail.com>
2022-03-29 01:43:26 +0200

    Fix test.c wrongly merged

--- compiler/test.c
+++ compiler/test.c
@@ -1,17 +1,6 @@
 #include <inttypes.h>
-<<<<<<< HEAD
-int main()
-{
-int a = 2;
-while(a!=8){
-    a = a +1;
-}
-return 5;
-=======
 
 int main() {
-    char d;
-    d = 1080;
-    return d;
->>>>>>> 531c500119504141fe103d7ecaa46da795563923
+    int a = 0;
+    return 0;
 }
