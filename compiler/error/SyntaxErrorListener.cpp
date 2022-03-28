65b0880b2c84bead138d25dda4fd30a63c30aff3
Allan Guigal <allan.guigal0@gmail.com>
2022-03-28 08:49:53 +0200

    Debug error compilation

--- compiler/error/SyntaxErrorListener.cpp
+++ compiler/error/SyntaxErrorListener.cpp
@@ -4,11 +4,7 @@
 
 #include "SyntaxErrorListener.h"
 
-void SyntaxErrorListener::throwSemanticError(Token* symbol, string message) {
-    cout << "Syntax error at " << symbol->getLine() << " " << message;
-}
-
 void SyntaxErrorListener::syntaxError(Recognizer *recognizer, Token *offendingSymbol, size_t line, size_t charPositionInLine,
 const std::string &msg, std::exception_ptr e) {
-    cout << "Syntax error" << endl
+    cout << "Syntax error" << endl;
 }
