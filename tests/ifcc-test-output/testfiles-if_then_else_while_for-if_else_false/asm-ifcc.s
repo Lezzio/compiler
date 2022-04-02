594d327c9a8fba43a082d6cc6bd6f72ea00513d8
JulienBvr <julien-bouvier@orange.fr>
2022-04-02 18:24:02 +0200

    test comparatives

--- tests/ifcc-test-output/testfiles-if_then_else_while_for-if_else_false/asm-ifcc.s
+++ tests/ifcc-test-output/testfiles-if_then_else_while_for-if_else_false/asm-ifcc.s
@@ -1,38 +1,179 @@
+going to L function :main
+About to add symbol name = main
+
+***   Actual Symbol Table   ***
+-------------------------------------------------------------------------------------------------------------
+|   Index   ;    Name   ;   Scope   ;   Type   ;   Additionnal   ;    State    ;    IsConst   | 
+-------------------------------------------------------------------------------------------------------------
+|  1  ;  getchar@PLT  ;  GLOBAL  ;  2  ;  0  ;  2  ;  0  | 
+|  1  ;  putchar@PLT  ;  GLOBAL  ;  3  ;  0  ;  2  ;  0  | 
+------------------------------------------------------------------------------------------------------------
+Added symbol finished
+Scope is = main_1
+Scope is = main_1
+About to L block
+ DEF AFFECTATION L 
+About to add symbol name = variable
+
+***   Actual Symbol Table   ***
+-------------------------------------------------------------------------------------------------------------
+|   Index   ;    Name   ;   Scope   ;   Type   ;   Additionnal   ;    State    ;    IsConst   | 
+-------------------------------------------------------------------------------------------------------------
+|  1  ;  getchar@PLT  ;  GLOBAL  ;  2  ;  0  ;  2  ;  0  | 
+|  1  ;  main  ;  GLOBAL  ;  0  ;  0  ;  2  ;  0  | 
+|  1  ;  putchar@PLT  ;  GLOBAL  ;  3  ;  0  ;  2  ;  0  | 
+------------------------------------------------------------------------------------------------------------
+Scope is = main_1
+Added symbol finished
+ POINT #1 
+Scope is = main_1
+ POINT #2 
+Scope is = main_1
+code = main_1_variable
+
+***   Actual Symbol Table   ***
+-------------------------------------------------------------------------------------------------------------
+|   Index   ;    Name   ;   Scope   ;   Type   ;   Additionnal   ;    State    ;    IsConst   | 
+-------------------------------------------------------------------------------------------------------------
+|  1  ;  getchar@PLT  ;  GLOBAL  ;  2  ;  0  ;  2  ;  0  | 
+|  1  ;  main  ;  GLOBAL  ;  0  ;  0  ;  2  ;  0  | 
+|  1  ;  putchar@PLT  ;  GLOBAL  ;  3  ;  0  ;  2  ;  0  | 
+|  4  ;  !tmp0  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  1  ;  variable  ;  main_1  ;  0  ;  0  ;  0  ;  0  | 
+------------------------------------------------------------------------------------------------------------
+ POINT #3 
+ END DEF AFFECTATION L 
+Scope is = main_1
+Scope is = main_1
+code = main_1_variable
+
+***   Actual Symbol Table   ***
+-------------------------------------------------------------------------------------------------------------
+|   Index   ;    Name   ;   Scope   ;   Type   ;   Additionnal   ;    State    ;    IsConst   | 
+-------------------------------------------------------------------------------------------------------------
+|  1  ;  getchar@PLT  ;  GLOBAL  ;  2  ;  0  ;  2  ;  0  | 
+|  1  ;  main  ;  GLOBAL  ;  0  ;  0  ;  2  ;  0  | 
+|  1  ;  putchar@PLT  ;  GLOBAL  ;  3  ;  0  ;  2  ;  0  | 
+|  4  ;  !tmp0  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  8  ;  !tmp1  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  1  ;  variable  ;  main_1  ;  0  ;  0  ;  0  ;  0  | 
+------------------------------------------------------------------------------------------------------------
+Scope is = main_1
+code = main_1_!tmp1
+
+***   Actual Symbol Table   ***
+-------------------------------------------------------------------------------------------------------------
+|   Index   ;    Name   ;   Scope   ;   Type   ;   Additionnal   ;    State    ;    IsConst   | 
+-------------------------------------------------------------------------------------------------------------
+|  1  ;  getchar@PLT  ;  GLOBAL  ;  2  ;  0  ;  2  ;  0  | 
+|  1  ;  main  ;  GLOBAL  ;  0  ;  0  ;  2  ;  0  | 
+|  1  ;  putchar@PLT  ;  GLOBAL  ;  3  ;  0  ;  2  ;  0  | 
+|  4  ;  !tmp0  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  8  ;  !tmp1  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  1  ;  variable  ;  main_1  ;  0  ;  0  ;  0  ;  0  | 
+------------------------------------------------------------------------------------------------------------
+Scope is = main_1
+Scope is = main_1
+Scope is = main_1
+Scope is = main_1
+Scope is = main_1
+Scope is = main_1
+code = main_1_variable
+
+***   Actual Symbol Table   ***
+-------------------------------------------------------------------------------------------------------------
+|   Index   ;    Name   ;   Scope   ;   Type   ;   Additionnal   ;    State    ;    IsConst   | 
+-------------------------------------------------------------------------------------------------------------
+|  1  ;  getchar@PLT  ;  GLOBAL  ;  2  ;  0  ;  2  ;  0  | 
+|  1  ;  main  ;  GLOBAL  ;  0  ;  0  ;  2  ;  0  | 
+|  1  ;  putchar@PLT  ;  GLOBAL  ;  3  ;  0  ;  2  ;  0  | 
+|  4  ;  !tmp0  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  8  ;  !tmp1  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  12  ;  !tmp2  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  16  ;  !tmp3  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  1  ;  variable  ;  main_1  ;  0  ;  0  ;  0  ;  0  | 
+------------------------------------------------------------------------------------------------------------
+Scope is = main_1
+Scope is = main_1
+code = main_1_variable
+
+***   Actual Symbol Table   ***
+-------------------------------------------------------------------------------------------------------------
+|   Index   ;    Name   ;   Scope   ;   Type   ;   Additionnal   ;    State    ;    IsConst   | 
+-------------------------------------------------------------------------------------------------------------
+|  1  ;  getchar@PLT  ;  GLOBAL  ;  2  ;  0  ;  2  ;  0  | 
+|  1  ;  main  ;  GLOBAL  ;  0  ;  0  ;  2  ;  0  | 
+|  1  ;  putchar@PLT  ;  GLOBAL  ;  3  ;  0  ;  2  ;  0  | 
+|  4  ;  !tmp0  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  8  ;  !tmp1  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  12  ;  !tmp2  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  16  ;  !tmp3  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  20  ;  !tmp4  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  1  ;  variable  ;  main_1  ;  0  ;  0  ;  0  ;  0  | 
+------------------------------------------------------------------------------------------------------------
+Scope is = main_1
+code = main_1_variable
+
+***   Actual Symbol Table   ***
+-------------------------------------------------------------------------------------------------------------
+|   Index   ;    Name   ;   Scope   ;   Type   ;   Additionnal   ;    State    ;    IsConst   | 
+-------------------------------------------------------------------------------------------------------------
+|  1  ;  getchar@PLT  ;  GLOBAL  ;  2  ;  0  ;  2  ;  0  | 
+|  1  ;  main  ;  GLOBAL  ;  0  ;  0  ;  2  ;  0  | 
+|  1  ;  putchar@PLT  ;  GLOBAL  ;  3  ;  0  ;  2  ;  0  | 
+|  4  ;  !tmp0  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  8  ;  !tmp1  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  12  ;  !tmp2  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  16  ;  !tmp3  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  20  ;  !tmp4  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  1  ;  variable  ;  main_1  ;  0  ;  0  ;  0  ;  0  | 
+------------------------------------------------------------------------------------------------------------
+Scope is = main_1
+end L of function :main
 .text
 .globl	main
  main: 
-.mainBB0:
+.main#0:
    #prologue
    pushq %rbp
    movq %rsp, %rbp
   subq $32, %rsp
+IR reg to asm
+reg = !tmp0
+code = main_1_!tmp0
+
+***   Actual Symbol Table   ***
+-------------------------------------------------------------------------------------------------------------
+|   Index   ;    Name   ;   Scope   ;   Type   ;   Additionnal   ;    State    ;    IsConst   | 
+-------------------------------------------------------------------------------------------------------------
+|  1  ;  getchar@PLT  ;  GLOBAL  ;  2  ;  0  ;  2  ;  0  | 
+|  1  ;  main  ;  GLOBAL  ;  0  ;  0  ;  2  ;  0  | 
+|  1  ;  putchar@PLT  ;  GLOBAL  ;  3  ;  0  ;  2  ;  0  | 
+|  24  ;  !retvalue  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  4  ;  !tmp0  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  8  ;  !tmp1  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  12  ;  !tmp2  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  16  ;  !tmp3  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  20  ;  !tmp4  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  1  ;  variable  ;  main_1  ;  0  ;  0  ;  0  ;  0  | 
+------------------------------------------------------------------------------------------------------------
    movl $1, -4(%rbp)
-   movl -4(%rbp), %eax
-   movl %eax, -28(%rbp)
-   movl $1, -8(%rbp)
-   movl -28(%rbp), %eax
-   cmpl -8(%rbp), %eax
-    setne %al
-    movzbl %al, %eax
-   movl %eax, -12(%rbp)
-   cmpl    $0, -12(%rbp)
-   je  .mainBB4
-.mainBB2:
-   movl $12, -16(%rbp)
-   movl -16(%rbp), %eax
-   movl %eax, -28(%rbp)
-   jmp   .mainBB3
-.mainBB4:
-   movl $9, -20(%rbp)
-   movl -20(%rbp), %eax
-   movl %eax, -28(%rbp)
-   jmp   .mainBB3
-.mainBB3:
-   movl -28(%rbp), %eax
-   movl %eax, -24(%rbp)
-   jmp   .mainBB1
-.mainBB1:
-   movl -24(%rbp), %eax
-   #epilogue
-   leave
-   ret
+Scope is = main_0
+code = main_0_variable
+
+***   Actual Symbol Table   ***
+-------------------------------------------------------------------------------------------------------------
+|   Index   ;    Name   ;   Scope   ;   Type   ;   Additionnal   ;    State    ;    IsConst   | 
+-------------------------------------------------------------------------------------------------------------
+|  1  ;  getchar@PLT  ;  GLOBAL  ;  2  ;  0  ;  2  ;  0  | 
+|  1  ;  main  ;  GLOBAL  ;  0  ;  0  ;  2  ;  0  | 
+|  1  ;  putchar@PLT  ;  GLOBAL  ;  3  ;  0  ;  2  ;  0  | 
+|  24  ;  !retvalue  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  4  ;  !tmp0  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  8  ;  !tmp1  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  12  ;  !tmp2  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  16  ;  !tmp3  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  20  ;  !tmp4  ;  main_1  ;  0  ;  0  ;  1  ;  0  | 
+|  1  ;  variable  ;  main_1  ;  0  ;  0  ;  0  ;  0  | 
+------------------------------------------------------------------------------------------------------------
+THE SYMBOL IS NULL
