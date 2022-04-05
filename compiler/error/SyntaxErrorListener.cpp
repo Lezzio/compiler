05e89d72e1810ff503e10fb9656a91b77841fea6
Allan Guigal <allan.guigal0@gmail.com>
2022-04-05 18:09:48 +0200

    Remove the syntax error debugging

--- compiler/error/SyntaxErrorListener.cpp
+++ compiler/error/SyntaxErrorListener.cpp
@@ -10,7 +10,6 @@ SyntaxErrorListener::SyntaxErrorListener() = default;
 
 void SyntaxErrorListener::syntaxError(Recognizer *recognizer, Token *offendingSymbol, size_t line, size_t charPositionInLine,
 const std::string &msg, std::exception_ptr e) {
-    cout << "Syntax error" << endl;
     auto * error = new Error(msg, line);
     ErrorManager::getInstance()->addError(error);
 }
